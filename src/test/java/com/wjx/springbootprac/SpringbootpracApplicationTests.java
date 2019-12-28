bc38142e17941252a9c1c3860f4e9409f156c71a
wjx <156717494@qq.com>
2019-12-28 12:39:37 +0800

    一波流

---
+++ src/test/java/com/wjx/springbootprac/SpringbootpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjx.springbootprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
