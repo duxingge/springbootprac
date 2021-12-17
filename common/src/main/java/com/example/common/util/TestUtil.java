e85eca33737d2ef1cb85235ee2c001aecdef81d9
jiaxing.wang <jiaxing.wang@advance.ai>
2021-12-17 17:13:19 +0800

    b3 u1

--- common/src/main/java/com/example/common/util/TestUtil.java
+++ common/src/main/java/com/example/common/util/TestUtil.java
@@ -8,5 +8,6 @@ public class TestUtil {
     public static void main(String[] args) {
         System.out.println("ss");
         System.out.println("ss2");
+        System.out.println("sss3");
     }
 }
