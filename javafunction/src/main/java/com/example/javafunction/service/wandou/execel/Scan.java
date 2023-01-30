144317ede3b99e203b61904a7b1c2761839b19a6
jiaxing.wang <jiaxing.wang@advance.ai>
2023-01-30 20:54:33 +0800

    add support code for wandou

--- javafunction/src/main/java/com/example/javafunction/service/wandou/execel/Scan.java
+++ javafunction/src/main/java/com/example/javafunction/service/wandou/execel/Scan.java
@@ -2,6 +2,7 @@ package com.example.javafunction.service.wandou.execel;
 
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.ResourceUtils;
 
@@ -17,6 +18,11 @@ public class Scan {
     static Map<String,Integer> maps = new HashMap<>();
     static Set<String> ketSet = null;
     static ArrayList<String> sortList;
+//    static final String scanPath= "classpath:csvserror";
+    static final String scanPath= "classpath:csvs";
+    static final String outFileName = "normal";
+    static Set<String> preLineSets = Sets.newHashSet("LV 24-Segment FS Values[%]","RV 24-Segment FS Values[%]","LV 24-Segment SI Values[%]","RV 24-Segment SI Values[%]","LV 24-Segment ED Values[mm]","RV 24-Segment ED Values[mm]");
+    static List<String> coreList = Lists.newArrayList("Segment 1;","Segment 2;","Segment 3;","Segment 4;","Segment 5;","Segment 6;","Segment 7;","Segment 8;","Segment 9;","Segment 10;","Segment 11;","Segment 12;","Segment 13;","Segment 14;","Segment 15;","Segment 16;","Segment 17;","Segment 18;","Segment 19;","Segment 20;","Segment 21;","Segment 22;","Segment 23;","Segment 24;");
     static int nameIndex = 0;
     static {
         maps.put("Name",3);
@@ -37,14 +43,54 @@ public class Scan {
         maps.put("OB_ZSCORE_VENTR_SV_KG.SV_KG_L;LV_ SV/KG[ml/kg]",3);
         maps.put("OB_ZSCORE_VENTR_CO.CO_L;LV_ CO[ml/min]",3);
         maps.put("OB_ZSCORE_VENTR_CO_KG.CO_KG_L;LV_ CO/KG[ml/min/kg]",3);
+        maps.put("LV_ ED Area",3);
+        maps.put("LV_ ED Length",3);
+        maps.put("LV_ ES Area",3);
+        maps.put("LV_ ES Length",3);
+        maps.put("LV_ EDV[ml]",3);
+        maps.put("LV_ ESV[ml]",3);
+        maps.put("RV_ ED Area",3);
+        maps.put("RV_ ED Length",3);
+        maps.put("RV_ ES Area",3);
+        maps.put("RV_ ES Length",3);
+        maps.put("OB_ZSCORE_GSI.GSI; 4CV GSI[]",3);
+        maps.put("RV_ FreeWall Strain[%]",3);
+        maps.put("LV_ MAPSE septal[mm]",3);
+        maps.put("LV_ MAPSE lateral[mm]",3);
+        maps.put("RV_ TAPSE lateral[mm]",3);
+        maps.put("LV_ FreeWall Strain[%]",3);
+        maps.put("LV_ Sept.Wall Strain[%]",3);
+        maps.put("RV_ Sept.Wall Strain[%]",3);
+        maps.put("LV_ Basal-Lat.Wall FS[%]",3);
+        maps.put("RV_ Basal-Lat.Wall FS[%]",3);
+        maps.put("LV_ Basal-Sep.Wall FS[%]",3);
+        maps.put("RV_ Basal-Sep.Wall FS[%]",3);
+        maps.put("LV_ Basal-Apical FS[%]",3);
+        maps.put("RV_ Basal-Apical FS[%]",3);
+        maps.put("LV_ Global Strain.Lt GLS %[%]",3);
+        maps.put("RV_ Global Strain.Rt GLS %[%]",3);
+        maps.put("RV_ FreeWall Strain.Rt FWS %[%]",3);
+        maps.put("LV_ FreeWall Strain.Lt FWS %[%]",3);
+        maps.put("LV_ Sept.Wall Strain.Lt SWS %[%]",3);
+        maps.put("RV_ Sept.Wall Strain.Rt SWS %[%]",3);
+
+
+
         ketSet = maps.keySet();
         sortList = Lists.newArrayList(ketSet);
-        for (int i = 0; i < sortList.size(); i++) {
-            if("Name".equals(sortList.get(i))) {
-                nameIndex = i;
+        sortList.sort(String::compareTo);
+        List<String> sortPreLineSets = Lists.newArrayList(preLineSets);
+        sortPreLineSets.sort(String::compareTo);
+        for (String preLine : sortPreLineSets) {
+            for (String core : coreList) {
+                maps.put(String.format("%s %s",preLine,core),3);
+                sortList.add(String.format("%s %s",preLine,core));
             }
         }
 
+        sortList.remove("Name");
+        sortList.add(0,"Name");
+        nameIndex = 0;
     }
 
     public static void main(String[] args) throws IOException {
@@ -61,7 +107,7 @@ public class Scan {
         List<List<Object>> outputDatalist = new ArrayList<List<Object>>();
 
 
-        File file = ResourceUtils.getFile("classpath:csvserror");
+        File file = ResourceUtils.getFile(scanPath);
 //        System.out.println(JsonUtil.toString(file.list()));
         String preLineText = "";
         for (File f : file.listFiles()) {
@@ -73,21 +119,21 @@ public class Scan {
             try (BufferedReader br = new BufferedReader(new FileReader(f))) {
                 while ((line = br.readLine()) != null) {
                     lineArr = line.split(SplitBy);
-                    String key = findKey(line, ketSet);
+                    String key = findKey(line, ketSet,preLineText);
                     if(StringUtils.isNotBlank(key)) {
 //                        System.out.println();
 //                        out.println(String.format("  %s :  %s ",key,describe(key,lineArr[maps.get(key)-1])));
                         List<Object> data=new ArrayList<Object>();
                         data.add(key);
-                        int index = maps.get(key) - 1;
+                        int index = maps.get(key)==null?2:maps.get(key) - 1;
                         if(lineArr.length>index) {
 //                            data.add(describe(key,lineArr.length>index?lineArr[maps.get(key)-1]:""));
-                            fileValues.put(key,describe(key,lineArr.length>index?lineArr[maps.get(key)-1]:""));
+                            fileValues.put(key,describe(key,lineArr.length>index?lineArr[index]:""));
                         }else {
-                            System.out.println("error column key "+ key + " file "+ f.getName());
+                            System.out.println("error column key "+ key +" line "+ line+  " file "+ f.getName());
                         }
                     }
-
+                    preLineText = updatePreLine(line,preLineText);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -97,11 +143,18 @@ public class Scan {
         }
 //        out.close();
         String path = "/Users/wangjiaxing/IdeaProjects/springbootprac";
-        String fileName = "exception";
+        String fileName = outFileName;
 
         File file1 = createCSVFile(exportData, outputDatalist, path, fileName);
         String fileName2 = file1.getName();
         System.out.println("文件名称：" + fileName2);
+    }
+
+    private static String updatePreLine(String currentLine,String preLine) {
+        if(StringUtils.isBlank(currentLine) || (!preLineHitSet(currentLine)) && currentLine.contains("Segment ")) {
+            return preLine;
+        }
+        return currentLine;
     }
 
     private static void sortList(List<List<Object>> outputDatalist) {
@@ -128,15 +181,29 @@ public class Scan {
         return value;
     }
 
-    private static String findKey(String lineArr, Set<String> keys) {
+    private static String findKey(String line, Set<String> keys,String preLine) {
         for (String key : keys) {
-            if(lineArr.contains(key)) {
+            if(line.contains(key)) {
                 return key;
             }
-        }
+            if(line.contains("Segment ") && preLineHitSet(preLine)) {
+                String preFix = preLineSets.stream().filter(it -> preLine.contains(it)).findAny().get();
+                if(key.contains(line.split(";")[1]+";") && key.contains(preFix)) {
+                        return key;
+                    }
+                }
+            }
         return null;
     }
 
+    private static boolean preLineHitSet(String preLine) {
+        for (String preLineSet : preLineSets) {
+            if(preLine.contains(preLineSet)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     /**
@@ -243,7 +310,6 @@ public class Scan {
             }
         }
 }
-
 
 
 
